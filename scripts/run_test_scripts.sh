a54dec95259ad648ffcec702c07321fea6eac8f0
Hiroki Tokunaga <tokusan441@gmail.com>
2022-11-27 16:40:34 +0900

    Add a comment

--- scripts/run_test_scripts.sh
+++ scripts/run_test_scripts.sh
@@ -3,6 +3,9 @@
 # Do not use `find` with the `-exec` option because it will not fail even if a
 # command fails. See
 # https://apple.stackexchange.com/questions/49042/how-do-i-make-find-fail-if-exec-fails.
+#
+# The last `(/)` limits a grob to list only directories. See
+# https://qiita.com/termoshtt/items/a99559dca654ff016b90.
 for dir in content/blog/*(/)
 do
     # The commands are enclosed in parentheses to prevent `cd` from affecting
